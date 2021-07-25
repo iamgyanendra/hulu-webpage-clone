e7f831a9e0a9da0d63bbeca3891c8214e6addd21
Gyandendra Prakash <prakash9.2020@gmail.com>
2021-07-26 02:52:37 +0530

    Done

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector('.modal')
+const loginBtn = document.querySelector('.login-btn')
+const closeBtn = document.querySelector('.close')
+
+loginBtn.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
+window.addEventListener('click', outSideClick) //Close login modal outside click
+
+
+function openModal(){
+    modal.style.display = 'block'
+}
+function closeModal(){
+    modal.style.display = 'none'
+}
+function outSideClick(e){
+    if(e.target == modal){
+        closeModal()
+    }
+}
